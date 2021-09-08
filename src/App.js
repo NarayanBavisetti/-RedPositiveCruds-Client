ae891339a212b465879f13be3f0ac3dbe97f8cbd
narayan <narayan3119@gmail.com>
2021-09-08 20:23:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import EmployeeList from './components/EmployeeList';
+
+
+function App() {
+  return (
+    <div className="container-xl">
+      <div className="table-responsive">
+        <div className="table-wrapper">
+           <EmployeeList />
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
